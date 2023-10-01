fb655fa83ac4c840368dd8594c25ab07a05afa7e
anisov <dmitriy.anisov@gs-labs.ru>
2023-10-01 21:27:16 +0300

    feat: Added hw02, unpack fanction

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,79 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func IsDigit(s string) bool {
+	for _, c := range s {
+		if c < '0' || c > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func prevIsNotEscape(prevValue string, prevIsString bool) bool {
+	return prevValue != `\` || prevIsString
+}
+
+func addSymbolInResult(result *strings.Builder, value string, isString bool) {
+	if isString {
+		result.WriteString(value)
+	}
+}
+
+func Unpack(data string) (string, error) {
+	var (
+		prevValue    string
+		prevIsString bool
+		result       strings.Builder
+	)
+	for i := 0; i < len(data); i++ {
+		currValue := string(data[i])
+		if IsDigit(currValue) && !prevIsString && prevValue != `\` {
+			return "", ErrInvalidString
+		}
+		if prevValue == `\` && !(IsDigit(currValue) || currValue == `\`) {
+			return "", ErrInvalidString
+		}
+		newValue := prevValue
+		if IsDigit(currValue) {
+			digit, _ := strconv.Atoi(currValue)
+			newValue = strings.Repeat(prevValue, digit)
+		}
+		addSymbolInResult(&result, newValue, prevIsString)
+		isString := true
+		if (IsDigit(currValue) || currValue == `\`) && prevIsNotEscape(prevValue, prevIsString) {
+			isString = false
+		}
+		prevIsString = isString
+		prevValue = currValue
+	}
+	addSymbolInResult(&result, prevValue, prevIsString)
+	return result.String(), nil
+}
+
+func UnpackFirst(data string) (string, error) {
+	var prevValue string
+	var result strings.Builder
+	for i := 0; i < len(data); i++ {
+		currValue := string(data[i])
+		if IsDigit(currValue) && (IsDigit(prevValue) || prevValue == "") {
+			return "", ErrInvalidString
+		}
+		newValue := prevValue
+		if IsDigit(currValue) {
+			digit, _ := strconv.Atoi(currValue)
+			newValue = strings.Repeat(prevValue, digit)
+			currValue = ""
+		}
+		result.WriteString(newValue)
+		prevValue = currValue
+	}
+	result.WriteString(prevValue)
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,17 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "aabc2", expected: "aabcc"},
+		{input: "aabc0", expected: "aab"},
+		{input: "aabc1", expected: "aabc"},
+		{input: "a0abc", expected: "abc"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `\4qwe`, expected: `4qwe`},
+		{input: `q\43we`, expected: `q444we`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +40,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "aaa10", "50ac", `50a\c`, `\a\c`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
