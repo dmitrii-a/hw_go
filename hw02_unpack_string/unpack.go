ffff85133eb5031d94fbcbff4d8edd8bac356c74
anisov <dmitriy.anisov@gs-labs.ru>
2023-10-07 18:36:41 +0300

    fix: Fixed algo

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -3,7 +3,6 @@ package hw02unpackstring
 import (
 	"errors"
 	"strings"
-	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
@@ -12,12 +11,11 @@ func prevIsNotEscape(prevValue rune, prevIsString bool) bool {
 	return prevValue != '\\' || prevIsString
 }
 
-func addSymbolInResult(result *strings.Builder, value []rune, isString bool) {
-	if isString {
-		for _, r := range value {
-			result.WriteRune(r)
-		}
+func IsDigit(s rune) bool {
+	if s < '0' || s > '9' {
+		return false
 	}
+	return true
 }
 
 func Unpack(s string) (string, error) {
@@ -28,23 +26,27 @@ func Unpack(s string) (string, error) {
 	)
 	backslash := '\\'
 	for _, value := range s {
-		if unicode.IsDigit(value) && !prevIsString && prevValue != backslash {
+		if IsDigit(value) && !prevIsString && prevValue != backslash {
 			return "", ErrInvalidString
 		}
-		if prevValue == backslash && !(unicode.IsDigit(value) || value == backslash) {
+		if prevValue == backslash && !(IsDigit(value) || value == backslash) {
 			return "", ErrInvalidString
 		}
 		newValue := []rune{prevValue}
-		if unicode.IsDigit(value) {
+		if IsDigit(value) {
 			size := int(value - '0')
 			newValue = make([]rune, size)
 			for i := 0; i < size; i++ {
 				newValue[i] = prevValue
 			}
 		}
-		addSymbolInResult(&result, newValue, prevIsString)
+		if prevIsString {
+			for _, r := range newValue {
+				result.WriteRune(r)
+			}
+		}
 		isString := true
-		if (unicode.IsDigit(value) || value == backslash) && prevIsNotEscape(prevValue, prevIsString) {
+		if (IsDigit(value) || value == backslash) && prevIsNotEscape(prevValue, prevIsString) {
 			isString = false
 		}
 		prevIsString = isString
@@ -53,35 +55,8 @@ func Unpack(s string) (string, error) {
 	if prevValue == backslash {
 		return "", ErrInvalidString
 	}
-	addSymbolInResult(&result, []rune{prevValue}, prevIsString)
+	if prevIsString {
+		result.WriteRune(prevValue)
+	}
 	return result.String(), nil
 }
-
-//func IsDigit(s string) bool {
-//	for _, c := range s {
-//		if c < '0' || c > '9' {
-//			return false
-//		}
-//	}
-//	return true
-//}
-//func UnpackFirst(s string) (string, error) {
-//	var prevValue string
-//	var result strings.Builder
-//	for _, v := range s {
-//		currValue := string(v)
-//		if IsDigit(currValue) && (IsDigit(prevValue) || prevValue == "") {
-//			return "", ErrInvalidString
-//		}
-//		newValue := prevValue
-//		if IsDigit(currValue) {
-//			digit, _ := strconv.Atoi(currValue)
-//			newValue = strings.Repeat(prevValue, digit)
-//			currValue = ""
-//		}
-//		result.WriteString(newValue)
-//		prevValue = currValue
-//	}
-//	result.WriteString(prevValue)
-//	return result.String(), nil
-//}
