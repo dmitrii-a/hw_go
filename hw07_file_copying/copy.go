f2ca1336b2667bc66dec8a9c839e2729694306c4
anisov <dmitriy.anisov@gs-labs.ru>
2023-12-08 16:55:25 +0300

    feat: Added hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"math"
+	"os"
 )
 
 var (
@@ -9,7 +13,87 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+const chunkSize = int64(1024)
+
+type srcParams struct {
+	limit     int64
+	offset    int64
+	chunkSize int64
+}
+
+func validate(src *os.File, offset int64, limit int64) (srcParams, error) {
+	params := srcParams{
+		limit:     limit,
+		offset:    offset,
+		chunkSize: chunkSize,
+	}
+	fileInfo, err := src.Stat()
+	if err != nil {
+		return params, err
+	}
+	size := fileInfo.Size()
+	if offset > size {
+		return params, ErrOffsetExceedsFileSize
+	}
+	if !fileInfo.Mode().IsRegular() {
+		return params, ErrUnsupportedFile
+	}
+	size -= offset
+	if limit == 0 || limit > size {
+		params.limit = size
+	}
+	if chunkSize > params.limit {
+		params.chunkSize = params.limit
+	}
+	return params, err
+}
+func openFiles(fromPath, toPath string) (*os.File, *os.File, error) {
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, 0744)
+	if err != nil {
+		return nil, nil, err
+	}
+	dst, err := os.OpenFile(toPath, os.O_CREATE|os.O_WRONLY, 0744)
+	if err != nil {
+		return nil, nil, err
+	}
+	return src, dst, nil
+}
+
+func execute(src, dst *os.File, params srcParams) error {
+	bar := pb.StartNew(int(math.Ceil(float64(params.limit) / float64(params.chunkSize))))
+	defer bar.Finish()
+	var count int64
+	for {
+		if count >= params.limit {
+			break
+		}
+		w, err := io.CopyN(dst, src, params.chunkSize)
+		count += w
+		if err != nil && !errors.Is(err, io.EOF) {
+			return err
+		}
+		bar.Increment()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+	}
 	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	src, dst, err := openFiles(fromPath, toPath)
+	defer src.Close()
+	defer dst.Close()
+	if err != nil {
+		return err
+	}
+	params, err := validate(src, offset, limit)
+	if err != nil {
+		return err
+	}
+	_, err = src.Seek(params.offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	return execute(src, dst, params)
 }
