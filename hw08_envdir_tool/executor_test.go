ae99bda96d5527c119d934f124dcbd15b7ada124
anisov <dmitriy.anisov@gs-labs.ru>
2023-12-15 00:34:51 +0300

    feat: Added hw08

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("run echo.sh with envs", func(t *testing.T) {
+		env := Environment{
+			"BAR":   {"bar", false},
+			"EMPTY": {"", false},
+			"FOO":   {"   foo\nwith new line", false},
+			"HELLO": {"\"hello\"", false},
+			"UNSET": {"", true},
+		}
+		code := RunCmd([]string{"./testdata/echo.sh", "1", "2"}, env)
+		require.Equal(t, code, OK)
+	})
+	t.Run("run echo.sh without env", func(t *testing.T) {
+		env := Environment{}
+		code := RunCmd([]string{"./testdata/echo.sh", "1", "2"}, env)
+		require.Equal(t, code, OK)
+	})
+	t.Run("run echo.sh without args", func(t *testing.T) {
+		env := Environment{}
+		code := RunCmd([]string{"./testdata/echo.sh"}, env)
+		require.Equal(t, code, OK)
+	})
 }
