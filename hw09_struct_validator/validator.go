e0b115605c69ca1aa75a1d45452e2eb4a1e58ea7
Dmitriy Anisov <dmitriyanisov@Mac-mini-Dmitriy.local>
2024-01-09 01:04:29 +0300

    refactor: Refactored code

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -79,118 +79,6 @@ func IsDigit(s string) bool {
 	return true
 }
 
-func processValidateField(
-	fieldValue reflect.Value,
-	fieldValidateFunc func(value reflect.Value) error,
-	validatorErrors ValidatorErrors,
-	validationErrors ValidationErrors,
-) (ValidationErrors, ValidatorErrors) {
-	err := fieldValidateFunc(fieldValue)
-	var vrError ValidatorError
-	var vnErrors ValidationErrors
-	switch {
-	case errors.As(err, &vrError):
-		validatorErrors = append(validatorErrors, vrError)
-	case errors.As(err, &vnErrors):
-		validationErrors = append(validationErrors, vnErrors...)
-	}
-	return validationErrors, validatorErrors
-}
-
-func validateField(fieldValue reflect.Value, name string, tags string) ValidationErrors {
-	var validationErrors ValidationErrors
-	var validatorErrors ValidatorErrors
-	for _, tag := range strings.Split(tags, separator) {
-		splitTag := strings.Split(tag, ":")
-		if len(splitTag) < 2 {
-			validatorErrors = append(validatorErrors, ValidatorError{Field: name, Err: ErrIncorrectValidator})
-		}
-		validator := splitTag[0]
-		validatorValue := splitTag[1]
-		fieldValidateFunc, validatorError := getFieldValidateFunc(name, validator, validatorValue)
-		if validatorError != nil {
-			validatorErrors = append(validatorErrors, *validatorError)
-			continue
-		}
-		if fieldValue.Kind() == reflect.Slice {
-			for i := 0; i < fieldValue.Len(); i++ {
-				validationErrors, validatorErrors = processValidateField(
-					fieldValue.Index(i), fieldValidateFunc, validatorErrors, validationErrors,
-				)
-			}
-		} else {
-			validationErrors, validatorErrors = processValidateField(
-				fieldValue, fieldValidateFunc, validatorErrors, validationErrors,
-			)
-		}
-	}
-	if len(validatorErrors) > 0 {
-		panic(validatorErrors)
-	}
-	return validationErrors
-}
-
-func getFieldValidateFunc(
-	name string,
-	validator,
-	validatorValue string,
-) (func(value reflect.Value) error, *ValidatorError) {
-	switch {
-	case validator == lenTagValidator:
-		length, err := strconv.Atoi(validatorValue)
-		if err != nil {
-			return nil, &ValidatorError{Field: name, Err: ErrInvalidLength}
-		}
-		return func(value reflect.Value) error {
-			return validateLenTag(name, length, value)
-		}, nil
-	case validator == regexpTagValidator:
-		re, err := regexp.Compile(validatorValue)
-		if err != nil {
-			return nil, &ValidatorError{Field: name, Err: ErrInvalidRegexp}
-		}
-		return func(value reflect.Value) error {
-			return validateRegexpTag(name, re, value)
-		}, nil
-	case validator == inTagValidator:
-		data := strings.Split(validatorValue, valueSeparator)
-		isDigitData := len(data) == 2 && IsDigit(data[0]) && IsDigit(data[1])
-		if isDigitData {
-			minValue, err := strconv.Atoi(data[0])
-			if err != nil {
-				return nil, &ValidatorError{Field: name, Err: fmt.Errorf("%w%v", ErrInvalidValue, data[0])}
-			}
-			maxValue, err := strconv.Atoi(data[1])
-			if err != nil {
-				return nil, &ValidatorError{Field: name, Err: fmt.Errorf("%w%v", ErrInvalidValue, data[1])}
-			}
-			return func(value reflect.Value) error {
-				return validateIntInTag(name, minValue, maxValue, value)
-			}, nil
-		}
-		return func(value reflect.Value) error {
-			return validateStringInTag(name, data, value)
-		}, nil
-	case validator == minTagValidator:
-		length, err := strconv.Atoi(validatorValue)
-		if err != nil {
-			return nil, &ValidatorError{Field: name, Err: ErrInvalidMin}
-		}
-		return func(value reflect.Value) error {
-			return validateMinTag(name, length, value)
-		}, nil
-	case validator == maxTagValidator:
-		length, err := strconv.Atoi(validatorValue)
-		if err != nil {
-			return nil, &ValidatorError{Field: name, Err: ErrInvalidMax}
-		}
-		return func(value reflect.Value) error {
-			return validateMaxTag(name, length, value)
-		}, nil
-	}
-	return nil, nil
-}
-
 func validateLenTag(name string, length int, fieldValue reflect.Value) error {
 	if fieldValue.Kind() != reflect.String {
 		return ValidatorError{Field: name, Err: ErrIncorrectType}
@@ -290,6 +178,118 @@ func validateMaxTag(name string, length int, fieldValue reflect.Value) error {
 				Field: name, Err: fmt.Errorf("%w%v", ErrValueMoreThanMax, length),
 			},
 		)
+	}
+	return validationErrors
+}
+
+func getFieldValidateFunc(
+	name string,
+	validator,
+	validatorValue string,
+) (func(value reflect.Value) error, *ValidatorError) {
+	switch {
+	case validator == lenTagValidator:
+		length, err := strconv.Atoi(validatorValue)
+		if err != nil {
+			return nil, &ValidatorError{Field: name, Err: ErrInvalidLength}
+		}
+		return func(value reflect.Value) error {
+			return validateLenTag(name, length, value)
+		}, nil
+	case validator == regexpTagValidator:
+		re, err := regexp.Compile(validatorValue)
+		if err != nil {
+			return nil, &ValidatorError{Field: name, Err: ErrInvalidRegexp}
+		}
+		return func(value reflect.Value) error {
+			return validateRegexpTag(name, re, value)
+		}, nil
+	case validator == inTagValidator:
+		data := strings.Split(validatorValue, valueSeparator)
+		isDigitData := len(data) == 2 && IsDigit(data[0]) && IsDigit(data[1])
+		if isDigitData {
+			minValue, err := strconv.Atoi(data[0])
+			if err != nil {
+				return nil, &ValidatorError{Field: name, Err: fmt.Errorf("%w%v", ErrInvalidValue, data[0])}
+			}
+			maxValue, err := strconv.Atoi(data[1])
+			if err != nil {
+				return nil, &ValidatorError{Field: name, Err: fmt.Errorf("%w%v", ErrInvalidValue, data[1])}
+			}
+			return func(value reflect.Value) error {
+				return validateIntInTag(name, minValue, maxValue, value)
+			}, nil
+		}
+		return func(value reflect.Value) error {
+			return validateStringInTag(name, data, value)
+		}, nil
+	case validator == minTagValidator:
+		length, err := strconv.Atoi(validatorValue)
+		if err != nil {
+			return nil, &ValidatorError{Field: name, Err: ErrInvalidMin}
+		}
+		return func(value reflect.Value) error {
+			return validateMinTag(name, length, value)
+		}, nil
+	case validator == maxTagValidator:
+		length, err := strconv.Atoi(validatorValue)
+		if err != nil {
+			return nil, &ValidatorError{Field: name, Err: ErrInvalidMax}
+		}
+		return func(value reflect.Value) error {
+			return validateMaxTag(name, length, value)
+		}, nil
+	}
+	return nil, nil
+}
+
+func processValidateField(
+	fieldValue reflect.Value,
+	fieldValidateFunc func(value reflect.Value) error,
+	validationErrors ValidationErrors,
+	validatorErrors ValidatorErrors,
+) (ValidationErrors, ValidatorErrors) {
+	err := fieldValidateFunc(fieldValue)
+	var vrError ValidatorError
+	var vnErrors ValidationErrors
+	switch {
+	case errors.As(err, &vrError):
+		validatorErrors = append(validatorErrors, vrError)
+	case errors.As(err, &vnErrors):
+		validationErrors = append(validationErrors, vnErrors...)
+	}
+	return validationErrors, validatorErrors
+}
+
+func validateField(fieldValue reflect.Value, name string, tags string) ValidationErrors {
+	var validationErrors ValidationErrors
+	var validatorErrors ValidatorErrors
+	for _, tag := range strings.Split(tags, separator) {
+		splitTag := strings.Split(tag, ":")
+		if len(splitTag) < 2 {
+			validatorErrors = append(validatorErrors, ValidatorError{Field: name, Err: ErrIncorrectValidator})
+		}
+		validator := splitTag[0]
+		validatorValue := splitTag[1]
+		fieldValidateFunc, validatorError := getFieldValidateFunc(name, validator, validatorValue)
+		if validatorError != nil {
+			validatorErrors = append(validatorErrors, *validatorError)
+			continue
+		}
+		if fieldValue.Kind() == reflect.Slice {
+			for i := 0; i < fieldValue.Len(); i++ {
+				validationErrors, validatorErrors = processValidateField(
+					fieldValue.Index(i), fieldValidateFunc, validationErrors, validatorErrors,
+				)
+			}
+		} else {
+			validationErrors, validatorErrors = processValidateField(
+				fieldValue, fieldValidateFunc, validationErrors, validatorErrors,
+			)
+		}
+	}
+	if len(validatorErrors) > 0 {
+		panic(validatorErrors)
 	}
 	return validationErrors
 }
