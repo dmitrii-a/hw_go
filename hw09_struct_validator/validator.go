e4c7ec66cc1a046f4501e23d2b2ddf9d2849e945
Dmitriy Anisov <dmitriyanisov@Mac-mini-Dmitriy.local>
2024-01-08 12:52:38 +0300

    feat: Added hw09

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,39 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	validateTag        = "validate"
+	separator          = "|"
+	valueSeparator     = ","
+	lenTagValidator    = "len:"
+	inTagValidator     = "in:"
+	regexpTagValidator = "regexp:"
+	minTagValidator    = "min:"
+	maxTagValidator    = "max:"
+)
+
+var (
+	ErrTypeNotStruct    = errors.New("type is not a struct")
+	ErrInvalidLength    = errors.New("invalid length")
+	ErrNotEqualLength   = errors.New("length not equal ")
+	ErrInvalidRegexp    = errors.New("invalid regexp")
+	ErrRegexpNotMatch   = errors.New("regexp not match ")
+	ErrInvalidValue     = errors.New("invalid value ")
+	ErrValueNotIn       = errors.New("value not in ")
+	ErrInvalidMin       = errors.New("invalid value for min validator")
+	ErrInvalidMax       = errors.New("invalid value for max validator")
+	ErrValueLessThanMin = errors.New("value less than ")
+	ErrValueMoreThanMax = errors.New("value more than ")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +42,177 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var validationError string
+	for _, err := range v {
+		validationError += fmt.Sprintf("%s: %s\n", err.Field, err.Err.Error())
+	}
+	return validationError
+}
+
+func IsDigit(s string) bool {
+	for _, c := range s {
+		if c < '0' || c > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func validateField(fieldValue reflect.Value, name string, tags string) ValidationErrors {
+	var validationErrors ValidationErrors
+	for _, tag := range strings.Split(tags, separator) {
+		switch {
+		case strings.HasPrefix(tag, lenTagValidator):
+			validationErrors = append(validationErrors, validateLenTag(name, tag, fieldValue)...)
+		case strings.HasPrefix(tag, regexpTagValidator):
+			validationErrors = append(validationErrors, validateRegexpTag(name, tag, fieldValue)...)
+		case strings.HasPrefix(tag, inTagValidator):
+			validationErrors = append(validationErrors, validateInTag(name, tag, fieldValue)...)
+		case strings.HasPrefix(tag, minTagValidator):
+			validationErrors = append(validationErrors, validateMinTag(name, tag, fieldValue)...)
+		case strings.HasPrefix(tag, maxTagValidator):
+			validationErrors = append(validationErrors, validateMaxTag(name, tag, fieldValue)...)
+		}
+	}
+	return validationErrors
+}
+
+func validateLenTag(name, tag string, fieldValue reflect.Value) ValidationErrors {
+	var validationErrors ValidationErrors
+	length, err := strconv.Atoi(strings.TrimPrefix(tag, lenTagValidator))
+	if err != nil {
+		validationErrors = append(validationErrors, ValidationError{Field: name, Err: ErrInvalidLength})
+	}
+	if fieldValue.Len() != length {
+		validationErrors = append(
+			validationErrors,
+			ValidationError{
+				Field: name, Err: fmt.Errorf("%w%v", ErrNotEqualLength, length),
+			},
+		)
+	}
+	return validationErrors
+}
+
+func validateInTag(name, tag string, fieldValue reflect.Value) ValidationErrors {
+	var validationErrors ValidationErrors
+	s := strings.Split(strings.TrimPrefix(tag, inTagValidator), valueSeparator)
+	isMatch := false
+	if len(s) == 2 && IsDigit(s[0]) && IsDigit(s[1]) {
+		minValue, err := strconv.Atoi(s[0])
+		if err != nil {
+			validationErrors = append(
+				validationErrors, ValidationError{
+					Field: name,
+					Err:   fmt.Errorf("%w%v", ErrInvalidValue, s[0]),
+				},
+			)
+		}
+		maxValue, err := strconv.Atoi(s[1])
+		if err != nil {
+			validationErrors = append(
+				validationErrors, ValidationError{
+					Field: name, Err: fmt.Errorf("%w%v", ErrInvalidValue, s[1]),
+				},
+			)
+		}
+		if fieldValue.Len() >= minValue && fieldValue.Len() <= maxValue {
+			isMatch = true
+		}
+	}
+	for _, v := range s {
+		if fmt.Sprint(fieldValue) == v {
+			isMatch = true
+		}
+	}
+	if !isMatch {
+		validationErrors = append(
+			validationErrors,
+			ValidationError{
+				Field: name, Err: fmt.Errorf("%w%v", ErrValueNotIn, s),
+			},
+		)
+	}
+	return validationErrors
+}
+
+func validateRegexpTag(name, tag string, fieldValue reflect.Value) ValidationErrors {
+	var validationErrors ValidationErrors
+	pattern := strings.TrimPrefix(tag, regexpTagValidator)
+	re, err := regexp.MatchString(pattern, fieldValue.String())
+	if err != nil {
+		validationErrors = append(validationErrors, ValidationError{Field: name, Err: ErrInvalidRegexp})
+	}
+	if !re {
+		validationErrors = append(
+			validationErrors,
+			ValidationError{
+				Field: name, Err: fmt.Errorf("%w%v", ErrRegexpNotMatch, pattern),
+			},
+		)
+	}
+	return validationErrors
+}
+
+func validateMinTag(name, tag string, fieldValue reflect.Value) ValidationErrors {
+	var validationErrors ValidationErrors
+	length, err := strconv.Atoi(strings.TrimPrefix(tag, minTagValidator))
+	if err != nil {
+		validationErrors = append(validationErrors, ValidationError{Field: name, Err: ErrInvalidMin})
+	}
+	if val, ok := fieldValue.Interface().(int); ok && val < length {
+		validationErrors = append(
+			validationErrors,
+			ValidationError{
+				Field: name, Err: fmt.Errorf("%w%v", ErrValueLessThanMin, length),
+			},
+		)
+	}
+	return validationErrors
+}
+
+func validateMaxTag(name, tag string, fieldValue reflect.Value) ValidationErrors {
+	var validationErrors ValidationErrors
+	length, err := strconv.Atoi(strings.TrimPrefix(tag, maxTagValidator))
+	if err != nil {
+		validationErrors = append(validationErrors, ValidationError{Field: name, Err: ErrInvalidMax})
+	}
+	if val, ok := fieldValue.Interface().(int); ok && val > length {
+		validationErrors = append(
+			validationErrors,
+			ValidationError{
+				Field: name, Err: fmt.Errorf("%w%v", ErrValueMoreThanMax, length),
+			},
+		)
+	}
+	return validationErrors
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	reflectValue := reflect.ValueOf(v)
+	if reflectValue.Kind() != reflect.Struct {
+		return ErrTypeNotStruct
+	}
+	validationErrors := ValidationErrors{}
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fieldValue := reflectValue.Field(i)
+		fieldType := reflectValue.Type().Field(i)
+		tag := fieldType.Tag.Get(validateTag)
+		if tag == "" {
+			continue
+		}
+		if fieldType.Type.Kind() == reflect.Slice {
+			for i := 0; i < fieldValue.Len(); i++ {
+				err := validateField(fieldValue.Index(i), fieldType.Name, tag)
+				validationErrors = append(validationErrors, err...)
+			}
+		} else {
+			err := validateField(fieldValue, fieldType.Name, tag)
+			validationErrors = append(validationErrors, err...)
+		}
+	}
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,11 +16,11 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
@@ -38,23 +41,130 @@ type (
 
 func TestValidate(t *testing.T) {
 	tests := []struct {
-		in          interface{}
-		expectedErr error
+		name           string
+		in             interface{}
+		expectedErr    error
+		inCorrectError bool
 	}{
 		{
-			// Place your code here.
+			name:        "not struct",
+			in:          "string, not struct",
+			expectedErr: ErrTypeNotStruct,
 		},
-		// ...
-		// Place your code here.
+		{
+			name: "correct User",
+			in: User{
+				ID:   "123456789012345678901234567890123456",
+				Name: "John", Age: 20,
+				Email:  "test@test.ru",
+				Role:   "admin",
+				Phones: []string{"89999999999"},
+				meta:   json.RawMessage(`{"test": "test"}`),
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "incorrect User",
+			in: User{
+				ID:     "123",
+				Name:   "John",
+				Age:    20,
+				Email:  "test@test.ru",
+				Role:   "admin",
+				Phones: []string{"89999999999"},
+				meta:   json.RawMessage(`{"test": "test"}`),
+			},
+			expectedErr: ValidationErrors{{Field: "ID", Err: ErrNotEqualLength}},
+		},
+		{
+			name: "incorrect User role",
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "John",
+				Age:    20,
+				Email:  "test@test.ru",
+				Role:   "test",
+				Phones: []string{"89999999999"},
+				meta:   json.RawMessage(`{"test": "test"}`),
+			},
+			expectedErr: ValidationErrors{{Field: "Role", Err: ErrValueNotIn}},
+		},
+		{
+			name: "incorrect all fields in User",
+			in: User{
+				ID:     "123",
+				Name:   "John",
+				Age:    2,
+				Email:  "test",
+				Role:   "test",
+				Phones: []string{"89999999999999999999"},
+				meta:   json.RawMessage(`{"test": "test"}`),
+			},
+			expectedErr: ValidationErrors{
+				{Field: "ID", Err: ErrNotEqualLength},
+				{Field: "Age", Err: ErrValueLessThanMin},
+				{Field: "Email", Err: ErrRegexpNotMatch},
+				{Field: "Role", Err: ErrValueNotIn},
+				{Field: "Phones", Err: ErrNotEqualLength},
+			},
+		},
+		{
+			name:        "correct length of App",
+			in:          App{Version: "1.0.0"},
+			expectedErr: nil,
+		},
+		{
+			name:        "incorrect length of App",
+			in:          App{Version: "1.0.0.0"},
+			expectedErr: ValidationErrors{{Field: "Version", Err: ErrNotEqualLength}},
+		},
+		{
+			name: "Token without validation",
+			in: Token{
+				Header:    nil,
+				Payload:   nil,
+				Signature: nil,
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "correct Response",
+			in: Response{
+				Code: 200,
+				Body: "",
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "incorrect Response",
+			in: Response{
+				Code: 700,
+				Body: "",
+			},
+			expectedErr: ValidationErrors{{Field: "Code", Err: ErrValueNotIn}},
+		},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
 			tt := tt
 			t.Parallel()
-
-			// Place your code here.
-			_ = tt
+			errFromValidate := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.NoError(t, errFromValidate, errFromValidate)
+			}
+			if tt.expectedErr != nil && errors.As(errFromValidate, &ValidationErrors{}) {
+				var validationErrors ValidationErrors
+				if errors.As(errFromValidate, &validationErrors) {
+					for i, err := range validationErrors {
+						var validationErrorsFromValidate ValidationErrors
+						if errors.As(errFromValidate, &validationErrorsFromValidate) {
+							require.Contains(t, validationErrorsFromValidate[i].Field, err.Field)
+							require.ErrorIs(t, validationErrorsFromValidate[i].Err, err.Err)
+						}
+					}
+				}
+			}
 		})
 	}
 }
