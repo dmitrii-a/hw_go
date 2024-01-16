85a106418cc2b1961246d7c2cab774bca9b4ebc4
Dmitriy Anisov <dmitriyanisov@Mac-mini-Dmitriy.local>
2024-01-17 01:51:20 +0300

    feat: Added hw11

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +16,82 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address    string
+	timeout    time.Duration
+	connection net.Conn
+	in         io.ReadCloser
+	out        io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *telnetClient) Connect() error {
+	if c.in == nil {
+		return fmt.Errorf("in is invalid")
+	}
+	if c.out == nil {
+		return fmt.Errorf("out is invalid")
+	}
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	c.connection = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func read(reader io.Reader) ([]byte, error) {
+	data := make([]byte, 1024)
+	n, err := reader.Read(data)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return nil, err
+		}
+		return nil, fmt.Errorf("receiving error: %w", err)
+	}
+	return data[:n], nil
+}
+
+func (c *telnetClient) Close() error {
+	return c.connection.Close()
+}
+
+func (c *telnetClient) Send() error {
+	reader := bufio.NewReader(c.in)
+	for {
+		data, err := read(reader)
+		if data == nil {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if _, err := c.connection.Write(data); err != nil {
+			return fmt.Errorf("sending error: %w", err)
+		}
+	}
+}
+
+func (c *telnetClient) Receive() error {
+	reader := bufio.NewReader(c.connection)
+	for {
+		data, err := read(reader)
+		if data == nil {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if _, err := c.out.Write(data); err != nil {
+			return fmt.Errorf("receiving error: %w", err)
+		}
+	}
+}
